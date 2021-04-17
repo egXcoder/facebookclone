114ff4d0d7840eaf5276af8bfe3c4d1688e196c2
Ahmed Ibrahim <ah.ib94@hotmail.com>
2021-04-17 16:53:57 +0200

    redirect after login/register

--- app/Http/Controllers/RecentLoginController.php
+++ app/Http/Controllers/RecentLoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\RecentLogin;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,7 +16,7 @@ class RecentLoginController extends Controller
             $new_recent = $recent->consumeAndGenerateNewInstance();
             RecentLogin::AddNewRecentLoginToCookie($new_recent);
             request()->session()->regenerate();
-            return redirect()->intended('/');
+            return redirect()->intended(RouteServiceProvider::HOME);
         }
     }
 
